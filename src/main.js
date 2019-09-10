00951039e560dfb9b571acf8bdb8cec2b3bc84f5
liu_yuxuan <liu_yxuuan@neusoft.com>
2019-09-10 19:20:07 +0800

    添加element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Button from 'element-ui'
 // 全部引入element-ui
 // import ElementUI from 'element-ui'
 // import 'element-ui/lib/theme-chalk/index.css'
@@ -10,6 +11,8 @@ import router from './router'
 // 按需引入element-ui 可只添加部分组件
 
 Vue.config.productionTip = false
+Vue.prototype.$ELEMENT = {size: 'small', zIndex: 3000}
+Vue.use(Button)
 
 /* eslint-disable no-new */
 new Vue({

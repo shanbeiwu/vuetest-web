cd665a4a1f10886b4a5c1ff84b48e31d16f7f532
liu_yuxuan <liu_yxuuan@neusoft.com>
2019-09-12 12:57:48 +0800

    添加element-ui

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,37 @@
 <template>
   <div id="app">
+    <div class="topmenu">
+      <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        active-text-color="#ffd04b"
+        @select="handleSelect">
+        <el-submenu index="1">
+          <template slot="title">处理中心</template>
+          <el-menu-item index="1-1">选项1</el-menu-item>
+          <el-menu-item index="1-2">选项2</el-menu-item>
+          <el-menu-item index="1-3">选项3</el-menu-item>
+          <el-menu-item index="1-4">选项4</el-menu-item>
+        </el-submenu>
+        <el-submenu index="2">
+          <template slot="title">我的工作台</template>
+          <el-menu-item index="2-1">选项1</el-menu-item>
+          <el-menu-item index="2-2">选项2</el-menu-item>
+          <el-menu-item index="2-3">选项3</el-menu-item>
+          <el-menu-item index="2-4">选项4</el-menu-item>
+          <!--<el-submenu index="2-4">-->
+            <!--<template slot="title">选项4</template>-->
+            <!--<el-menu-item index="2-4-1">选项1</el-menu-item>-->
+            <!--<el-menu-item index="2-4-2">选项2</el-menu-item>-->
+            <!--<el-menu-item index="2-4-3">选项3</el-menu-item>-->
+          <!--</el-submenu>-->
+        </el-submenu>
+        <el-menu-item index="3">消息中心</el-menu-item>
+        <el-menu-item index="4">订单管理</el-menu-item>
+      </el-menu>
+      <div class="line"></div>
+    </div>
     <!--<img src="./assets/logo.png">-->
     <router-view/>
   </div>
@@ -7,17 +39,44 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      activeIndex2: '1'
+    }
+  },
+  methods: {
+    handleSelect (key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 0px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 0px;
+  }
+  body {
+    margin-top: 0px;
+    margin-right: 0px;
+    margin-left: 0px;
+    margin-bottom: 0px;
+  }
+  /*.el-menu--horizontal>.el-menu-item {*/
+    /*float: right;*/
+    /*width: 120px;*/
+  /*}*/
+  /*.el-menu--horizontal>.el-submenu {*/
+    /*float: right;*/
+    /*width: 120px;*/
+  /*}*/
+  /*div.el-menu--horizontal {*/
+    /*width: 120px!important;*/
+  /*}*/
 </style>
